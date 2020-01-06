bda3e8557d91b2c846d80cea5e702b45a1052119
Sistla-Harini <59256357+Sistla-Harini@users.noreply.github.com>
2020-01-06 03:24:17 -0800

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,55 +1,51 @@
-class Employee
+package com.deloitte.firstmvn.hibfirst.entity;
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Employee 
 {
-private int empid;
-private String ename;
-double salary;
-public Employee(){}
-public Employee(int empid,String ename, double salary)
-{
-this.empid=empid;
-this.ename=ename;
-this.salary=salary;
-}
-public void setEmpid(int empid)
-{
-this.empid=empid;
-}
-public void setEname(String ename)
-{
-this.ename=ename;
-}
-public void setSalary(double salary)
-{
-this.salary=salary;
-}
-public int getEmpid()
- {
-return empid;
-}
-public String getEname()
-{
-return ename;
-}
-public double getSalary()
-{
-return salary;
-}
-}
-class Test
-{
-public static void main(String[]args)
-{
-Employee e=new Employee(101,"ajay",25000);
-System.out.println("Empid is "+e.getEmpid());
-System.out.println("ename is "+e.getename());
-System.out.println("salary is "+e.getsalary());
-}
-}
+	@Id
+	int empid;
+	String ename;
+	double salary;
+	Date doj;
+	public Employee() {}
+	public Employee(int empid, String ename, double salary, Date doj) {
+		super();
+		this.empid = empid;
+		this.ename = ename;
+		this.salary = salary;
+		this.doj = doj;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Date getDoj() {
+		return doj;
+	}
+	public void setDoj(Date doj) {
+		this.doj = doj;
+	}
+	
+	
 
-
-
-
-
+}
 
 
 
